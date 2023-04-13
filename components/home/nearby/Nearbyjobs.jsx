bec45d531597cdbec829ad002db8bf868e2de697
Tassio-Med <tassiohfmed@gmail.como>
2023-04-13 17:38:38 -0300

    :sparkles: feat: componente Nearby estruturado e estilizado

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,60 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+// import React from 'react'
+// import { View, Text } from 'react-native'
 
-import styles from './nearbyjobs.style'
+// import styles from './nearbyjobcard.style'
 
-const Nearbyjobs = () => {
+// const NearbyJobCard = () => {
+//   return (
+//     <View>
+//       <Text>NearbyJobCard</Text>
+//     </View>
+//   )
+// }
+
+// export default NearbyJobCard
+
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import styles from './nearbyjobcard.style'
+import { COLORS } from '../../../constants'
+import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
+import useFetch from '../../../hook/useFetch';
+
+const NearbyJobs = () => {
+  const router = useRouter();
+  
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React developer',
+    num_pages: 1
+  })
+
   return (
-    <View>
-      <Text>Nearbyjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary}/>
+        ) : error ? (
+          <Text>Something went wrog</Text>
+        ) : (
+          data?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+            />
+          ))
+        )}
+
+      </View>
     </View>
   )
 }
 
-export default Nearbyjobs
+export default NearbyJobs
