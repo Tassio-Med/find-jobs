dada309c5848a3f1ac2aede0882e9c0433191817
Tassio-Med <tassiohfmed@gmail.como>
2023-04-14 18:22:23 -0300

    :sparkles: feat: implementando funções de pesquisa

--- app/index.js
+++ app/index.js
@@ -8,6 +8,7 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 
 const Home = () => {
   const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
@@ -31,7 +32,16 @@ const Home = () => {
             padding: SIZES.medium
           }}
         >
-          <Welcome/>
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if(searchTerm) {
+                router.push(`/search/${searchTerm}`)
+              }
+               
+            }}
+          />
           <Popularjobs/>
           <Nearbyjobs/>
         </View>
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -15,7 +15,7 @@ import { icons, SIZES } from '../../../constants';
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleCLick }) => {
   const router = useRouter();
   const [activeJobType, setActiveJobType] = useState('Full-time')
 
@@ -31,13 +31,13 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=''
-            onChange={() => {}}
+            value={searchTerm}
+            onChange={(text) => setSearchTerm(text)}
             placeholder='What are you looking for?'
           />
         </View>
 
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleCLick}>
           <Image
             source={icons.search}
             resizeMode='contain'
