a4c70849ec56630c72665848fa87a13c088c8bc8
Tassio-Med <tassiohfmed@gmail.como>
2023-04-14 16:46:25 -0300

    :sparkles: feat: puxando elementos para a página com id específico.

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -13,12 +13,77 @@ import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } fro
 import { COLORS, SIZES, icons } from '../../constants';
 import useFetch from '../../hook/useFetch';
 
+
+const tabs = ["About", "Qualifications", "Responsabilities"];
+
 const JobDetails = () => {
   const params = useSearchParams();
   const router = useRouter();
 
+  const { data, isLoading, error, refetch } = useFetch('job-details', {
+    job_id: params.job_id
+  })
+
+  const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const onRefresh = () => {}
+
   return (
-    <Text>Job Details</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUr={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUr={icons.share}
+              dimension="60%"
+            />
+          ),
+          headerTitle:''
+        }}
+      />
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false} 
+          refreshControl={<RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            />}
+        >
+          {isLoading ? (
+            <ActivityIndicator size={large} color={COLORS.primary}/>
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            </View>
+          )}
+        </ScrollView>
+      </>
+    </SafeAreaView>
   )
 }
 
